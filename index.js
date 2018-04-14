5d201895c5094ef8de15072139ae93a17ead14fc
Dean Harris <dean.harris@comparethemarket.com.au>
2018-04-14 23:52:05 +1000

    Changes implementation from everything in the global context to class
    This allows reuse of the Counter class to initiate as many counters as
    desired.
    
    Also made some minor changes to the markup to allow this to function as
    intended and also be laid out nicely on the page

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
-let el = {
-  lessBtn: document.getElementById('remove-btn'),
-  repeatBtn: document.getElementById('reset-btn'),
-  moreBtn: document.getElementById('add-btn'),
-  count: document.getElementById('count')
-};
+class Counter {
+	constructor( target ) {
+		this.container = document.querySelector( target );
+		this.decrement = this.container.querySelector( '.counter-decrement' );
+		this.increment = this.container.querySelector( '.counter-increment' );
+		this.display = this.container.querySelector( '.counter-display' );
+		this.reset = this.container.querySelector( '.counter-reset' );
+		this.count = 0;
 
-let count = 0; 
+		this.updateDisplay = this.updateDisplay.bind( this );
+		this.decrementCount = this.decrementCount.bind( this );
+		this.incrementCount = this.incrementCount.bind( this );
+		this.resetCount = this.resetCount.bind( this );
 
-function add() {
-  count = count + 1;
-  el.count.innerText = count;
+		this.increment.addEventListener( 'click', this.incrementCount );
+		this.decrement.addEventListener( 'click', this.decrementCount );
+		this.reset.addEventListener( 'click', this.resetCount );
+	}
+
+	updateDisplay() {
+		this.display.innerHTML = this.count;
+	}
+
+	decrementCount() {
+		this.count --;
+		this.updateDisplay();
+	};
+
+	incrementCount() {
+		this.count++;
+		this.updateDisplay();
+	};
+
+	resetCount() {
+		this.count = 0;
+		this.updateDisplay();
+	}
+
+
 }
-el.moreBtn
 
-function remove() {
-  count--;
-  el.count.innerText = count;
-}
+new Counter( '.counter-1' );
+new Counter( '.counter-2' );
 
-function reset() {
-  count = 0;
-  el.count.innerText = count;
-}
-
-el.moreBtn.addEventListener('click', function() {
-  add();
-}, false);
-
-el.lessBtn.addEventListener('click', function() {
-  remove();
-}, false);
-
-el.repeatBtn.addEventListener('click', function() {
-  reset();
-}, false);
